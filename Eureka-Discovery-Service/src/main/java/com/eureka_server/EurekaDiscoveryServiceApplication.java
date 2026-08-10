9923ac10a41f193b5606fc47c87c1d37f9fdec8c
Deepak Kumar Singh <dks913536@gmail.com>
2026-08-10 14:31:51 +0530

    Initial project setup

---
+++ Eureka-Discovery-Service/src/main/java/com/eureka_server/EurekaDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaDiscoveryServiceApplication.class, args);
+	}
+
+}
