4068ec649d281ae3442fd5fdaecda02285230c35
Deepak Kumar Singh <dks913536@gmail.com>
2026-08-11 14:08:32 +0530

    Doctor service

---
+++ Doctor-Service/src/main/java/com/doctor_service/entity/State.java
@@ -0,0 +1,32 @@
+package com.doctor_service.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "state")
+public class State {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id",nullable = false)
+    private Long id;
+
+    @Column(name = "state", nullable = false)
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
