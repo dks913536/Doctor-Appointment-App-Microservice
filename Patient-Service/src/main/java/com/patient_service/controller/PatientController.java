41fb58024429af3610648b6367bf203c2c5c7a1e
Deepak Kumar Singh <dks913536@gmail.com>
2026-09-15 09:42:28 +0530

    Patient Service

--- Patient-Service/src/main/java/com/patient_service/controller/PatientController.java
+++ Patient-Service/src/main/java/com/patient_service/controller/PatientController.java
@@ -3,10 +3,9 @@ package com.patient_service.controller;
 import com.patient_service.entity.Patient;
 import com.patient_service.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/patient")
@@ -14,6 +13,14 @@ public class PatientController {
 
     @Autowired
     private PatientRepository patientRepository;
+
+    @PostMapping("/create")
+    public ResponseEntity<Patient> createPatient(@RequestBody Patient patient) {
+
+        Patient savedPatient = patientRepository.save(patient);
+
+        return new ResponseEntity<>(savedPatient, HttpStatus.CREATED);
+    }
 
     @GetMapping("/getpatientbyid")
     public Patient getPatientById(@RequestParam long id){
