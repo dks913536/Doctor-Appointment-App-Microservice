691c750796df4f28b6fb2cc9fd42886ab394a1cb
Deepak Kumar Singh <dks913536@gmail.com>
2026-09-21 13:55:22 +0530

    Payment Service

--- Payment-service/src/main/java/com/payment_service/client/BookingClient.java
+++ Payment-service/src/main/java/com/payment_service/client/BookingClient.java
@@ -3,10 +3,15 @@ package com.payment_service.client;
 import com.payment_service.dto.BookingConfirmation;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "BOOKING-SERVICE",url = "http://localhost:9091/api/v1/booking")
 public interface BookingClient {
+
     @GetMapping("/getBookingById")
     public BookingConfirmation getBookingById(@RequestParam long bookingId);
+
+    @PutMapping("/update-booking")
+    public String updateBookingStatus(@RequestParam long bookingId);
 }
